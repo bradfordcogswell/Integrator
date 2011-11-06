9a250602fd51ae975c1bf1126dfdac96b757c08a
Brad Cogswell <brad.cogswell@compuware.com>
2011-11-05 23:41:05 -0400

    Completed the processing of commits into the database.  Next activities:
    1. Use connection string from database
    2. Modify the spInsertCommit to only insert when the hash does not exist.
    3. Begin processing the commits into VersionOne.

---
+++ IntegratorData/IntegratorData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Practices.EnterpriseLibrary.Data;
+
+namespace IntegratorData
+{
+    public class IntegratorData
+    {
+        Database integrator;
+
+        public bool GetRepositories(string RepositoryTypeName)
+        {
+            integrator = DatabaseFactory.CreateDatabase("integrator");
+
+            return true;
+        }
+
+    }
+}
--- IntegratorWindows/Integrator.cs
+++ IntegratorWindows/Integrator.cs
@@ -6,11 +6,14 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
 
 namespace IntegratorWindows
 {
     public partial class Integrator : Form
     {
+
         public Integrator()
         {
             InitializeComponent();
@@ -25,29 +28,92 @@ namespace IntegratorWindows
             PollRepositories();
         }
 
+        public SqlDataReader GetRepositories()
+        {
+            string sqlconnectionstring = "Database=integrator;Server=(local);uid=integrator;pwd=Compuware1!;";
+            SqlConnection con = new SqlConnection(sqlconnectionstring);
+            string sp = "[dbo].[spGetRespositories]";
+            SqlCommand com = new SqlCommand(sp, con);
+            com.CommandType = CommandType.StoredProcedure;
+            SqlDataReader reader = null;
+            try
+            {
+                con.Open();
+                reader = com.ExecuteReader();
+            }
+            catch (System.Exception ex)
+            {
+                string exmessage = ex.Message;
+                //log the exception
+            }
+            return reader;
+        }
+
+        public string InsertCommit(string RepositoryId,string Hash,string AuthorName,string AuthorEmail,string Message,string CommitDate)
+        {
+            string sqlconnectionstring = "Database=integrator;Server=(local);uid=integrator;pwd=Compuware1!;";
+            SqlConnection con = new SqlConnection(sqlconnectionstring);
+            string sp = "[dbo].[spInsertCommit]";
+            SqlCommand com = new SqlCommand(sp, con);
+            com.CommandType = CommandType.StoredProcedure;
+            try
+            {
+                con.Open();
+                com.Parameters.AddWithValue("@RepositoryId",RepositoryId);
+                com.Parameters.AddWithValue("@Hash", Hash);
+                com.Parameters.AddWithValue("@AuthorName", AuthorName);
+                com.Parameters.AddWithValue("@AuthorEmail", AuthorEmail);
+                com.Parameters.AddWithValue("@Message", Message);
+                com.Parameters.AddWithValue("@CommitDate", CommitDate);
+                com.ExecuteNonQuery();
+            }
+            catch (System.Exception ex)
+            {
+                string exmessage = ex.Message;
+                //log the exception
+            }
+            return sp;
+        }
+
         public bool PollRepositories()
         {
 
-            string url;
+            SqlDataReader reader = null;
 
-            url = "d:\\projects\\bradstest\\.git";
-
-            var git_url = GitSharp.Repository.FindRepository(url);
-            if (git_url == null || !GitSharp.Repository.IsValid(git_url))
+            try
             {
-                Console.Write("Given path doesn't seem to refer to a git repository: " + url);
-                return false;
+                reader = GetRepositories();
+                while (reader.Read())
+                {
+                    var repositoryid = reader["RepositoryId"];
+                    string repositoryidstring = repositoryid.ToString();
+                    var repositorylocation = reader["RepositoryLocation"];
+                    string repositorylocationstring = repositorylocation.ToString();
+
+                    var git_url = GitSharp.Repository.FindRepository(repositorylocationstring);
+                    if (git_url == null || !GitSharp.Repository.IsValid(git_url))
+                    {
+                        Console.Write("Given path doesn't seem to refer to a git repository: " + repositorylocation);
+                        return false;
+                    }
+                    var repo = new GitSharp.Repository(git_url);
+                    PollCommitments(repo, repositoryidstring);
+                }
             }
-            var repo = new GitSharp.Repository(git_url);
-            PollCommitments(repo);
+            catch (System.Exception ex)
+            {
+                string exmessage = ex.Message;
+                //log the exception
+            }
             return true;
         }
 
-        private void PollCommitments(GitSharp.Repository repo)
+        private void PollCommitments(GitSharp.Repository repo, string RepositoryId)
         {
 
             int i = 0;
             string name = string.Empty;
+            string hash = string.Empty;
             string authorname = string.Empty;
             string authoremail = string.Empty;
             string message = string.Empty;
@@ -67,11 +133,12 @@ namespace IntegratorWindows
                 var c1 = new GitSharp.Commit(repo, name);
                 try
                 {
+                    hash = c1.Hash;
                     authorname = c1.Author.Name;
                     authoremail = c1.Author.EmailAddress;
                     message = c1.Message;
                     commitdate = c1.CommitDate;
-                    //Load to Database
+                    InsertCommit(RepositoryId,hash,authorname,authoremail,message,commitdate.DateTime.ToString());
                 }
                 catch
                 {
